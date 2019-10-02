7b748e810809ea87d7059216369380f1172d420b
Hubert <hubert.wawrzacz@gmail.com>
2019-10-02 20:28:19 +0200

    Add login panel - without functionality

--- mgrTaskFrontend/src/app/app.module.ts
+++ mgrTaskFrontend/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './Components/login-page/login-page.component';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
